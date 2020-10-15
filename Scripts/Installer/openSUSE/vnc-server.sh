8f99db9f8bd7699353c0a12434e19ba7f12a7a63
radumamy <63680669+radumamy@users.noreply.github.com>
2020-10-15 16:38:02 +0100

    Update vnc-server.sh

--- Scripts/Installer/openSUSE/vnc-server.sh
+++ Scripts/Installer/openSUSE/vnc-server.sh
@@ -15,18 +15,13 @@ chmod +x /usr/local/bin/vncserver-stop
 echo " "
 echo "You can now start vncserver by running vncserver-start"
 echo " "
-echo "It will ask you to enter a password when first time starting it."
+echo "It will ask you to enter a password when starting it for the first time."
 echo " "
 echo "The VNC Server will be started at 127.0.0.1:5901"
 echo " "
-echo "You can connect to this address with a VNC Viewer you prefer"
+echo "You can connect to this address with a VNC Viewer client of your choice"
 echo " "
-echo "Connect to this address will open a window with Xfce4 Desktop Environment"
-echo " "
-echo " "
-echo " "
-echo "Running vncserver-start"
-echo " "
+echo "Connecting to this address will open a window running Xfce4 Desktop Environment"
 echo " "
 echo " "
 echo "To Kill VNC Server just run vncserver-stop"
@@ -36,5 +31,3 @@ echo " "
 
 echo "export DISPLAY=":1"" >> /etc/profile
 source /etc/profile
-
-vncserver-start
