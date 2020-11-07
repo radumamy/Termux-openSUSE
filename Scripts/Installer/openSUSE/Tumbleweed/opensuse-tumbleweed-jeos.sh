701419a187bc2e8f42f432d153a4932c2e333967
radumamy <63680669+radumamy@users.noreply.github.com>
2020-11-07 08:31:28 +0000

    Update opensuse-tumbleweed-jeos.sh

--- Scripts/Installer/openSUSE/Tumbleweed/opensuse-tumbleweed-jeos.sh
+++ Scripts/Installer/openSUSE/Tumbleweed/opensuse-tumbleweed-jeos.sh
@@ -104,6 +104,6 @@ echo "echo You can now use openSUSE Tumbleweed. To close it type exit and to lau
 clear
 echo "Starting Setup"
 echo "bash ~/setup
-switch-user; exit" > $folder/root/.bash_profile
+switch-user; " > $folder/root/.bash_profile
 echo " "
 bash $bin 
