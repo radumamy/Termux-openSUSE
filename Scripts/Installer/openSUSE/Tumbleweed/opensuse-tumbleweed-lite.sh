2aceeb6da48f5972511ecbfd01829574309bea0a
radumamy <63680669+radumamy@users.noreply.github.com>
2021-02-27 00:56:24 +0000

    Update opensuse-tumbleweed-lite.sh

--- Scripts/Installer/openSUSE/Tumbleweed/opensuse-tumbleweed-lite.sh
+++ Scripts/Installer/openSUSE/Tumbleweed/opensuse-tumbleweed-lite.sh
@@ -24,8 +24,8 @@ if [ "$first" != 1 ];then
 		*)
 			echo "unknown architecture"; exit 1 ;;
 		esac
-#                wget "http://download.opensuse.org/ports/aarch64/tumbleweed/appliances/opensuse-tumbleweed-image.aarch64-lxc.tar.xz" -O $tarball
-		wget "http://download.opensuse.org/ports/aarch64/tumbleweed/appliances/openSUSE-Tumbleweed-ARM-JeOS.aarch64-rootfs.aarch64.tar.xz" -O $tarball
+                wget "http://download.opensuse.org/ports/aarch64/tumbleweed/appliances/opensuse-tumbleweed-image.aarch64-lxc.tar.xz" -O $tarball
+#		wget "http://download.opensuse.org/ports/aarch64/tumbleweed/appliances/openSUSE-Tumbleweed-ARM-JeOS.aarch64-rootfs.aarch64.tar.xz" -O $tarball
 	fi
 	cur=`pwd`
 	mkdir -p "$folder"
@@ -38,9 +38,9 @@ if [ "$first" != 1 ];then
 	echo "127.0.0.1 localhost" > etc/hosts
     echo "nameserver 8.8.8.8" > etc/resolv.conf
     echo "nameserver 8.8.4.4" >> etc/resolv.conf
-    echo "Patching Yast"
-    sed -i '59,59 s/^/#/' usr/sbin/yast2
-    sed -i '66,69 s/^/#/' usr/sbin/yast2
+#    echo "Patching Yast"
+#    sed -i '59,59 s/^/#/' usr/sbin/yast2
+#    sed -i '66,69 s/^/#/' usr/sbin/yast2
 	cd "$cur"
 fi
 mkdir -p opensuse-tumbleweed-binds
